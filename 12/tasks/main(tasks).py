887d0c0592402a278565ea0cba51ed0d583d6599
Doviaks <dovydas.nar@gmail.com>
2025-01-30 21:07:06 +0200

    try, except, else, finally task

--- 12/tasks/main(tasks).py
+++ 12/tasks/main(tasks).py
@@ -17,27 +17,46 @@ print('===============================')
 
 # task 3
 
+# while True:
+#
+#     skaicius_ivestis =  input('Iveskite skaiciu: ')
+#     daliklis_ivestis =  input('Iveskite dalikli: ')
+#
+#     try:
+#         skaicius = int(skaicius_ivestis)
+#         daliklis = int(daliklis_ivestis)
+#
+#         res = skaicius / daliklis
+#
+#         print(f'Rezultatas yra: {res}')
+#     except ValueError:
+#         print(f'Crash su ValueError, nes ivedete {skaicius_ivestis} ir {daliklis_ivestis}')
+#     except ZeroDivisionError:
+#         print(f'Crash su ZeroDivisionError, skaicius {skaicius_ivestis} nesidalina is {daliklis_ivestis} ')
+#
+#     break
+
+print('===============================')
+print('===============================')
+
+#                   task 3
+
+
 while True:
-
-    skaicius_ivestis =  input('Iveskite skaiciu: ')
-    daliklis_ivestis =  input('Iveskite dalikli: ')
-
+    ivestis = input('Iveskite skaiciu: ')
     try:
-        skaicius = int(skaicius_ivestis)
-        daliklis = int(daliklis_ivestis)
-
-        res = skaicius / daliklis
-
-        print(f'Rezultatas yra: {res}')
+        res = int(ivestis)
+        print(res)
     except ValueError:
-        print(f'Crash su ValueError, nes ivedete {skaicius_ivestis} ir {daliklis_ivestis}')
-    except ZeroDivisionError:
-        print(f'Crash su ZeroDivisionError, skaicius {skaicius_ivestis} nesidalina is {daliklis_ivestis} ')
+        print(f'Tu gavai ValueError')
+    else:
+        print(f'Konversija sekminga: {res}')
+    finally:
+        print(f'Programa baige savo darba')
+        break
 
-    # break
-
-
-
+print('===============================')
+print('===============================')
 
 
 
