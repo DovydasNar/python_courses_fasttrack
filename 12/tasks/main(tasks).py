08565fdd9b5f6e4dc59d74c4610b8112b0e4c691
Doviaks <dovydas.nar@gmail.com>
2025-01-30 19:37:46 +0200

    3 task

--- 12/tasks/main(tasks).py
+++ 12/tasks/main(tasks).py
@@ -12,6 +12,31 @@ print(dalinti(10, 2))
 print(dalinti(5, 0))
 print(dalinti(8, 4))
 
+print('===============================')
+print('===============================')
+
+# task 3
+
+while True:
+
+    skaicius_ivestis =  input('Iveskite skaiciu: ')
+    daliklis_ivestis =  input('Iveskite dalikli: ')
+
+    try:
+        skaicius = int(skaicius_ivestis)
+        daliklis = int(daliklis_ivestis)
+
+        res = skaicius / daliklis
+
+        print(f'Rezultatas yra: {res}')
+    except ValueError:
+        print(f'Crash su ValueError, nes ivedete {skaicius_ivestis} ir {daliklis_ivestis}')
+    except ZeroDivisionError:
+        print(f'Crash su ZeroDivisionError, skaicius {skaicius_ivestis} nesidalina is {daliklis_ivestis} ')
+
+    # break
+
+
 
 
 
