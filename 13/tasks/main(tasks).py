b36294c5b0eeb6000310a8fa98eceb7a7ff3999a
Doviaks <dovydas.nar@gmail.com>
2025-02-03 19:21:07 +0200

    2,3 tasks

--- 13/tasks/main(tasks).py
+++ 13/tasks/main(tasks).py
@@ -1,5 +1,5 @@
 #                                   1 task
-
+import datetime
 from datetime import datetime as d
 
 dabartinis_laikas = d.today()
@@ -16,5 +16,28 @@ sekunde = dabartinis_laikas.second
 print(f'Dabar yra: {valanda}:{minute}, {diena}-{menuo}-{metai}')
 
 
+print('=================================================')
+
 #                                   2 task
+
+data_object = datetime.datetime(1995, 7, 14, 15, 30, 00)
+print(data_object)
+
+data_object2 = datetime.datetime(2023, 1,1)
+print(data_object2)
+
+
+print('=================================================')
+
+#                                   3 task
+
+data_2000 = datetime.datetime(2000, 1, 1)
+
+data_from_today = (d.today() - data_2000).days
+
+print(f'Praejo {data_from_today} dienu nuo 2000-01-01')
+
+
+print('=================================================')
+
 
