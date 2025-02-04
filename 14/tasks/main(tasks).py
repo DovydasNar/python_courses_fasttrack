ff2937c268592bc55b7d10008b7f36f073406c21
Doviaks <dovydas.nar@gmail.com>
2025-02-04 20:37:12 +0200

    4,5,6 tasks

--- 14/tasks/main(tasks).py
+++ 14/tasks/main(tasks).py
@@ -14,7 +14,8 @@ print(Bike.manufacturer)
 
 print('===============================================')
 
-                                        # 2, 3 task
+                                        # 2, 3, 4, 5, 6 tasks
+from datetime import datetime
 
 class Book:
 
@@ -25,11 +26,41 @@ class Book:
         self.author = author
         self.year = year
 
+    def get_book_age(self):
+        current_year = datetime.now().year
+        return current_year - self.year
+
+
 book1 = Book('Altoriu Sesely', 'Vincas Mykolaitis Putinas', 1933)
 book2 = Book('Vakaru fronte nieko naujo', 'E. M. Remarkas', 1928)
 book3 = Book('Tarp pilku debesu', 'Ruta Sepetys', 2023)
 
+
 print(f'Book 1: {book1.title}, {book1.author}, {book1.year}, publisher: {Book.publisher}')
 print(f'Book 2: {book2.title}, {book2.author}, {book2.year}')
 print(f'Book 3: {book3.title}, {book3.author}, {book3.year}')
+
+print('===============================================')
+
+print(f'Knyga: {book1.title} yra {book1.get_book_age()} metu senumo')
+print(f'Knyga: {book2.title} yra {book2.get_book_age()} metu senumo')
+print(f'Knyga: {book3.title} yra {book3.get_book_age()} metu senumo')
+
+print('===============================================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
