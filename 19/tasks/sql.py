fe1f3ed584dbd2072aa09f9defe9c738baef6037
Doviaks <dovydas.nar@gmail.com>
2025-02-13 16:07:13 +0200

    19 paskaita sql 4 task

--- 19/tasks/sql.py
+++ 19/tasks/sql.py
@@ -34,12 +34,31 @@ def rodyti_mokyklas(min_mokiniu_skaicius=0):
     for mokykla in mokyklos:
         print(f'Mokykla: {mokykla[0]}, Adresas: {mokykla[1]}, mokiniu skaicius: {mokykla[2]}')
 
+def atnaujinti_mokiniu_skaiciu(pavadinimas, naujas_mokiniu_skaicius):
+    cursor.execute('''
+    UPDATE mokykla
+    SET mokiniu_skaicius = ?
+    WHERE pavadinimas = ?
+    ''', (naujas_mokiniu_skaicius, pavadinimas))
+
+    if cursor.rowcount > 0:
+        print(f'Mokiniu skaicius mokykloje:{pavadinimas} sekmingai atnaujintas i {naujas_mokiniu_skaicius}')
+    else:
+        print(f'Mokykla su pavadinimu {pavadinimas} nerasta.')
+
+
+
 
 print('Visos mokyklos:')
 rodyti_mokyklas()
 
 print('Mokyklos su daugiau nei 600 zmoniu:')
 rodyti_mokyklas(600)
+
+
+atnaujinti_mokiniu_skaiciu('Kauno gimnazija', 1200)
+
+rodyti_mokyklas()
 
 
 
